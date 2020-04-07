ec63688e084acf6dd320f0fe4ab42812949e15b9
LongLiang <945812281@qq.com>
2020-04-08 00:04:33 +0800

    LinkedList

--- LinkedList/src/LinkedListDemo.java
+++ LinkedList/src/LinkedListDemo.java
@@ -14,7 +14,7 @@ public class LinkedListDemo {
             System.out.println(linkedList);
         }
 
-        linkedList.add(2, 666);
+        /*linkedList.add(2, 666);
         System.out.println(linkedList);
 
         linkedList.remove(2);
@@ -24,9 +24,37 @@ public class LinkedListDemo {
         System.out.println(linkedList);
 
         linkedList.removeLast();
-        System.out.println(linkedList);
+        System.out.println(linkedList);*/
 
 
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+        int count = sum(nums, 10);
+        System.out.println(count);
 
+    }
+
+    public static int sum(int[] nums, int l) {
+
+
+        return l == 0 ? 0 : nums[l - 1] + sum(nums, l - 1);
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        head.next = removeElements(head.next, val);
+
+        return head.val == val ? head.next : head;
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
