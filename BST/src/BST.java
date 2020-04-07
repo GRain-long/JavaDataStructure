e83f10fdf3596aed6ba39da34e8c63280f1ce9ed
LongLiang <945812281@qq.com>
2020-04-08 00:04:08 +0800

    Binary Search Tree

---
+++ BST/src/BST.java
@@ -0,0 +1,37 @@
+/**
+ * @Classname BST
+ * @Description 二分搜索树
+ * @Date 2020/4/7
+ * @Author Grain Rain
+ */
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+
+    private Node root;
+
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
