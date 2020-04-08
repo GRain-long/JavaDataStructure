42a4569bf96a342cb496bf6a736700af8f41a012
LongLiang <945812281@qq.com>
2020-04-08 23:32:09 +0800

    Binary Search Tree

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @Classname BST
  * @Description 二分搜索树
@@ -34,4 +38,350 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+
+    /**
+     * 向二分搜索树中添加新的元素e
+     *
+     * @param e
+     */
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+
+    /**
+     * 向以node为根的二分搜索树中插入元素e，递归算法
+     *
+     * @param node
+     * @param e
+     * @return 返回插入新节点后二分搜索树的根
+     */
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    /**
+     * 判断二分搜索树中是否包含元素e
+     *
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    /**
+     * 判断以node为根的二分搜索树中是否包含元素e
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+        if (e.compareTo(node.e) == 0) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    /**
+     * 二分搜索树层序遍历
+     */
+    public void levelOrder() {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+
+            Node cur = queue.remove();
+
+            System.out.println(cur.e);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+    }
+
+    /**
+     * 二分搜索树的前序遍历
+     */
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    /**
+     * 非递归的前序遍历
+     */
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+    }
+
+
+    /**
+     * 前序遍历以node为根的二分搜索树的递归算法
+     *
+     * @param node
+     */
+    private void preOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    /**
+     * 二分搜索树的中序遍历
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    /**
+     * 中序遍历以node为根的二分搜索树
+     *
+     * @param node
+     */
+    private void inOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    /**
+     * 二分搜索树的后序遍历
+     */
+    public void postOrder() {
+        preOrder(root);
+    }
+
+
+    /**
+     * 后序遍历以node为根的二分搜索树
+     *
+     * @param node
+     */
+    private void postOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        preOrder(node.left);
+        preOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    /**
+     * 返回二分搜索树的最小值
+     *
+     * @return
+     */
+    public E minimum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return minimum(root).e;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最小值的节点
+     *
+     * @param node
+     * @return
+     */
+    private Node minimum(Node node) {
+        return node.left == null ? node : minimum(node.left);
+    }
+
+    /**
+     * 返回二分搜索树的最大值
+     *
+     * @return
+     */
+    public E maximum() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return maximum(root).e;
+    }
+
+    /**
+     * 返回以node为根的二分搜索树的最大值的节点
+     *
+     * @param node
+     * @return
+     */
+    private Node maximum(Node node) {
+        return node.right == null ? node : minimum(node.right);
+    }
+
+
+    /**
+     * 删除二分搜索树的最小值的节点
+     *
+     * @return
+     */
+    public E removeMin() {
+        E min = minimum();
+        root = removeMin(root);
+        return min;
+    }
+
+    /**
+     * 删除二分搜索树以node为最小值的节点
+     *
+     * @param node
+     * @return 返回删除节点后的新的二分搜索树的根
+     */
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 删除二分搜索树的最大值的节点
+     *
+     * @return
+     */
+    public E removeMax() {
+        E min = maximum();
+        root = removeMax(root);
+        return min;
+    }
+
+    /**
+     * 删除二分搜索树以node为最大值的节点
+     *
+     * @param node
+     * @return 返回删除节点后的新的二分搜索树的根
+     */
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    /**
+     * 从二分搜索树种删除元素为e的节点
+     *
+     * @param e
+     */
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    /**
+     * 删除以node为根节点的元素为e的节点
+     *
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node remove(Node node, E e) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (node.e.compareTo(e) > 0) {
+            node.left = remove(node.left, e);
+            return node;
+        } else if (node.e.compareTo(e) < 0) {
+            node.right = remove(node.right, e);
+            return node;
+        } else {
+            //如果左子树为空
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            //如果右子树为空
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+            //node的后继
+            Node successor = minimum(node.right);
+            //把删除node.right的后继后的二叉树赋值给后继的right
+            successor.right = removeMin(node.right);
+            //把node.left赋值给后继的left
+            successor.left = node.left;
+
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
